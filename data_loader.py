7f495bd63bb01896f742e3e9db006290d7f23e15
unknown <gosiaszwed17@gmail.com>
2023-02-08 10:15:04 +0100

    add metadata loading

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,27 @@
+import os
+import pandas as pd
+
 DATA_ROOT_DIR = "data"
 
 
 class DataLoader:
     def __init__(self):
-        pass
+        self.metadata = pd.read_excel(os.path.join(DATA_ROOT_DIR, "sustained_polish/UPDRS_description.xlsx"), nrows=28)
+
+        self.recording_no, self.disease_length, self.age = None, None, None
+        self.updrs_off, self.updrs_30, self.updrs_60, self.updrs_120, self.updrs_180 = None, None, None, None, None
+
+        self.prepare_labels()
+
+    def prepare_labels(self):
+        self.recording_no = self.metadata["recording no"]
+        self.age = self.metadata["age"]
+        self.updrs_off = self.metadata["UPDRS OFF"]
+        self.updrs_30 = self.metadata["UPDRS 30"]
+        self.updrs_60 = self.metadata["UPDRS 60"]
+        self.updrs_120 = self.metadata[" UPDRS 120"]
+        self.updrs_180 = self.metadata[" UPDRS 180"]
+        self.disease_length = self.metadata["disease length [years]"]
 
     def load_data(self):
         pass
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from data_loader import DataLoader
 
 if __name__ == "__main__":
     print("Run Parkinson Disease detection")
+    data_loader = DataLoader()
